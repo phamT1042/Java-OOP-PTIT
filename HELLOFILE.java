36c29e487028ec091c6424270bd14e9c2f874afc
VanTienDesktop <pvantien203@gmail.com>
2023-08-15 16:25:54 +0700

    Hello File

---
+++ HELLOFILE.java
@@ -0,0 +1,24 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+public class HELLOFILE {
+    public static void main(String[] args) {
+        FileReader fp;
+        try {
+            fp = new FileReader("Hello.txt");
+            try {
+                BufferedReader read = new BufferedReader(fp);
+                String a;
+                while ((a = read.readLine()) != null) 
+                    System.out.println(a);
+                fp.close();
+                read.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
