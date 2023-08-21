18eac2cc1def75b6a2e747640c96a8b3fde3e0f0
VanTienDesktop <pvantien203@gmail.com>
2023-08-21 16:55:10 +0700

    Danh sách kề

--- J02105.java
+++ J02105.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class a {
+public class J02105 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
