30a99b5211f929e6e3e3a3e17477298bdd1d07bf
VanTienDesktop <pvantien203@gmail.com>
2023-09-19 22:31:49 +0700

    Bài toán tuyển sinh

--- J04013.java
+++ J04013.java
@@ -10,10 +10,12 @@ class ThiSinh {
         this.a = a;
         this.b = b;
         this.c = c;
+
         if (this.ma.charAt(2) == '1') this.uuTien = 0.5;
         else if (this.ma.charAt(2) == '2') this.uuTien = 1.0;
         else this.uuTien = 2.5;
         this.tongDiem = 2 * this.a + this.b + this.c;
+        
         if (this.tongDiem + this.uuTien >= 24) this.trangThai = "TRUNG TUYEN";
         else this.trangThai = "TRUOT";
     }
@@ -35,7 +37,7 @@ class ThiSinh {
 
 }
 
-public class a {
+public class J04013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
