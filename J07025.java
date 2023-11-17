c843ce59e1598cf92499a3c617979471f9c99626
VanTien <pvantien203@gmail.com>
2023-11-17 10:02:36 +0700

    Danh sách khách hàng trong file

---
+++ J07025.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+class KhachHang implements Comparable<KhachHang> {
+    private String maKH, tenKH, gioiTinh;
+    private String diaChi;
+    private Date ngaySinh;
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public KhachHang(int ma, String tenKH, String gioiTinh, String ngaySinh, String diaChi) {
+        this.maKH = String.format("KH%03d", ma);
+        this.tenKH = chuanHoaTen(tenKH);
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        try {
+            this.ngaySinh = sdf.parse(ngaySinh);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String chuanHoaTen(String s) {
+        String res = "";
+        String[] tach = s.split("\\s+");
+        for (String x : tach) 
+            res += x.substring(0, 1).toUpperCase() + x.substring(1).toLowerCase() + " ";
+        return res.trim();
+    }
+
+    @Override
+    public int compareTo(KhachHang o) {
+        return (this.ngaySinh.getTime() > o.ngaySinh.getTime() ? 1 : -1);
+    }
+
+    @Override
+    public String toString() {
+        return maKH + " " + tenKH + " " + gioiTinh + " " + diaChi
+                + " " + sdf.format(ngaySinh);
+    }
+}
+
+public class J07025 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("DATA.txt"));
+
+        int n = Integer.parseInt(sc.nextLine());
+        KhachHang listKH[] = new KhachHang[n];
+        for (int i = 0; i < n; i++) 
+            listKH[i] = new KhachHang(i + 1, sc.nextLine().trim(), sc.nextLine(), sc.nextLine(), sc.nextLine());
+
+        Arrays.sort(listKH);
+        for (KhachHang kh : listKH) System.out.println(kh);        
+
+        sc.close();
+    }
+}
