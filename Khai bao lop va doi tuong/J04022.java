add20188ba70fdf156db44d6c39e0af184345a83
VanTienDesktop <pvantien203@gmail.com>
2023-12-03 17:55:13 +0700

    Update 03/12

--- Khai bao lop va doi tuong/J04022.java
+++ Khai bao lop va doi tuong/J04022.java
@@ -43,11 +43,4 @@ public class J04022 {
         System.out.println(s1.union(s2));
         System.out.println(s1.intersection(s2));
     }
-    public static void main7927629(String[] args) {
-        Scanner in = new Scanner(System.in);
-        WordSet s1 = new WordSet(in.nextLine());
-        WordSet s2 = new WordSet(in.nextLine());
-        System.out.println(s1.union(s2));
-        System.out.println(s1.intersection(s2));
-    }
 }
