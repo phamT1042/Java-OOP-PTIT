036ee6134c8a17b07ab0239f67517904fb051665
VanTienDesktop <pvantien203@gmail.com>
2023-12-18 15:41:12 +0700

    Update 18/12

--- Note.java
+++ Note.java
@@ -5,15 +5,16 @@ public class Note {
         //sort array reverse: thêm Collections.reverseOrder() và dùng kiểu dữ liệu Integer/Long/...
         //Dùng LinkedList thay cho queue: push() - removeLast()
         //change element in arraylist: set()
+
         //Tách từ nhiều kí tự khác nhau: split("[.,?!]")
         //Tách từ nhiều dấu cách: split("\\s+") | Bỏ cách 2 đầu: trim()
-        //Đọc file nhị phân: DataInputStream input = new DataInputStream(new FileInputStream("DATA.in"));
         //Format mã: String.format("KH%02d", KhachHang.STT);
+        //làm tròn 1 số phần thập phân (sum là double): Math.round((sum / n) * 10) / 10.0
         //String -> Date: new SimpleDateFormat("dd/MM/yyyy").parse(ngaySinh)
         //Date -> String: new SimpleDateFormat("dd/MM/yyyy").format(ngaySinh)
         //Khoảng cách 2 date: TimeUnit.MILLISECONDS.toDays(end.getTime() - start.getTime());
-
-        //làm tròn 1 số phần thập phân (sum là double): Math.round((sum / n) * 10) / 10.0
+        
+        //Đọc file nhị phân: DataInputStream input = new DataInputStream(new FileInputStream("DATA.in"));
         //Đọc Object trong file nhị phân (thường là ArrayList):
         // ObjectInputStream inp = new ObjectInputStream(new FileInputStream("DATA.in"));
         // ArrayList<Integer> a = (ArrayList<Integer>) inp.readObject();
